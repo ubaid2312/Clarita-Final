472329458bb74a662d98f051c8207cb756499fa0
ubaid2312 <ubaidmolani92@gmail.com>
2026-06-11 18:43:53 +0500

    navbar function

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import HeroContent from "../components/HeroContent";
 import CTAButton from "../components/CTAButton";
 import ReviewBadge from "../components/RevivewBadge";
 import '../styles/hero.css';
-
+import '../styles/navbar.css';
+    
 
 function Home() {
     return (
